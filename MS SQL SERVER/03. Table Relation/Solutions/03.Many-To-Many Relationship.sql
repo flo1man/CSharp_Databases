8eab4243f487ca3adb9c8075def1728b278d69f2
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-02-25 13:51:17 +0200

    Initial commit

---
+++ MS SQL SERVER/03. Table Relation/Solutions/03.Many-To-Many Relationship.sql
@@ -0,0 +1,50 @@
+﻿CREATE DATABASE Relations
+USE Relations
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(20) NOT NULL
+)
+
+
+INSERT INTO Students
+		([Name])
+	VALUES
+		('Mila'),
+		('Toni'),
+		('Ron')
+
+
+CREATE TABLE Exams
+(
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+
+INSERT INTO Exams
+		([Name])
+	VALUES
+		('SpringMVC'),
+		('Neo4j'),
+		('Oracle 11g')
+
+
+CREATE TABLE StudentsExams
+(
+	 StudentID INT REFERENCES Students(StudentID),
+	 ExamID INT REFERENCES Exams(ExamID),
+	 CONSTRAINT PK_StudentsExams -- създаваме двоен PK, с връзка много към много
+		PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT INTO StudentsExams
+		(StudentID, ExamID)
+	VALUES
+		(1,101),
+		(1,102),
+		(2,101),
+		(3,103),
+		(2,102),
+		(2,103)
---
+++ MS SQL SERVER/03. Table Relation/Solutions/06.University Database.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE University
+USE University
+
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber INT UNIQUE NOT NULL,
+	StudentName VARCHAR(50) NOT NULL,
+	MajorID INT
+)
+
+
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+ALTER TABLE Students
+ADD FOREIGN KEY (MajorID) REFERENCES Majors(MajorID)
+
+
+CREATE TABLE Payments
+(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATE NOT NULL,
+	PaymentAmount DECIMAL(7,2) NOT NULL,
+	StudentID INT REFERENCES Students(StudentID) NOT NULL
+)
+
+
+CREATE TABLE Subjects
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT REFERENCES Students(StudentID),
+	SubjectID INT REFERENCES Subjects(SubjectID),
+	CONSTRAINT PK_StudentSubject 
+	PRIMARY KEY (StudentID, SubjectID)
+)
