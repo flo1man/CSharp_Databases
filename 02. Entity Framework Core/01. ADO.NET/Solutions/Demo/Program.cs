5da955f782c6f942023a568ab47fe92e9acb2880
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-03-21 18:06:04 +0200

    Rename folders and Initial commit

---
+++ 02. Entity Framework Core/01. ADO.NET/Solutions/Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // XML <coonection> <server>.</server> <database>SoftUni</database> </connection>
+            // JSON { "server": "."; "database": "SoftUni" }
+
+            using (var connection = new SqlConnection
+                ("Server=.;Integrated Security=true;Database=SoftUni;Encrypt=false")) //Encrypt=false ???
+            {
+                connection.Open();
+                string query1 = "UPDATE Employees SET Salary = Salary + 0.35";
+                string query2 = "SELECT COUNT(*) FROM Employees";
+                SqlCommand command1 = new SqlCommand(query1, connection);
+                SqlCommand command2 = new SqlCommand(query2, connection);
+                //var rowsAffected1 = command1.ExecuteNonQuery();
+                //var rowsAffected2 = (int)command2.ExecuteScalar();
+
+                //Console.WriteLine(rowsAffected1);
+                //Console.WriteLine(rowsAffected2);
+
+
+                var sqlCommand = new SqlCommand("SELECT * FROM Employees", connection);
+                using (SqlDataReader sqlReader = sqlCommand.ExecuteReader())
+                {
+                    while (sqlReader.Read())
+                    {
+                        Console.WriteLine($"{sqlReader["FirstName"]} {sqlReader["MiddleName"]} {sqlReader["LastName"]}");
+                        //Console.WriteLine($"{sqlReader[1]} {sqlReader[3]} {sqlReader[2]}");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ 02. Entity Framework Core/01. ADO.NET/Solutions/P01_InitialSetup/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace P01_InitialSetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (var connection = new SqlConnection
+            //    ("Server=.;Integrated Security=true;Database=master;Encrypt=false"))
+            //{
+            //    connection.Open();
+
+            //    //Create DATABASE
+            //    string createQuery = "CREATE DATABASE MinionsDB";
+            //    SqlCommand command = new SqlCommand(createQuery, connection);
+            //    command.ExecuteNonQuery();
+            //}
+
+            using (var connection = new SqlConnection
+                ("Server=.;Integrated Security=true;Database=MinionsDB;Encrypt=false"))
+            {
+                connection.Open();
+
+                // Create Countries
+                string tableCountries = "CREATE TABLE Countries(Id INT PRIMARY KEY IDENTITY," +
+                    " Name VARCHAR(100) NOT NULL)";
+                SqlCommand tableCountriesCommand = new SqlCommand(tableCountries, connection);
+                tableCountriesCommand.ExecuteNonQuery();
+
+                // Create Towns
+                string tableTowns = "CREATE TABLE Towns(Id INT PRIMARY KEY IDENTITY," +
+                    " CountryCode INT REFERENCES Countries(Id) NOT NULL)";
+                SqlCommand tableTownsCommand = new SqlCommand(tableTowns, connection);
+                tableTownsCommand.ExecuteNonQuery();
+
+                // Create Minions
+                string tableMinions = "CREATE TABLE Minions(Id INT PRIMARY KEY IDENTITY, Name VARCHAR(50) NOT NULL," +
+                    " Age INT, TownId INT REFERENCES Towns(Id) NOT NULL)";
+                SqlCommand tableMinionsCommand = new SqlCommand(tableMinions, connection);
+                tableMinionsCommand.ExecuteNonQuery();
+
+                // Create EvilnessFactors
+                string tableEvilnessFactors = "CREATE TABLE EvilnessFactors(Id INT PRIMARY KEY IDENTITY," +
+                    " Name VARCHAR(100) NOT NULL)";
+                SqlCommand tableEFCommand = new SqlCommand(tableEvilnessFactors, connection);
+                tableEFCommand.ExecuteNonQuery();
+
+                // Create Villains
+                string tableVillains = "CREATE TABLE Villains(Id INT PRIMARY KEY IDENTITY," +
+                    " Name VARCHAR(100) NOT NULL, EvilnessFactorId INT REFERENCES EvilnessFactors(Id))";
+                SqlCommand tableVillainsCommand = new SqlCommand(tableVillains, connection);
+                tableVillainsCommand.ExecuteNonQuery();
+
+                // Create MinionsVillains
+                string tableMinionsVillains = "CREATE TABLE MinionsVillains(MinionId INT REFERENCES Minions(Id)," +
+                    " VillainId INT REFERENCES Villains(Id), PRIMARY KEY(MinionId, VillainId))";
+                SqlCommand tableMVCommand = new SqlCommand(tableMinionsVillains, connection);
+                tableMVCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
